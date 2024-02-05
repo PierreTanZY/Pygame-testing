25839dc9f552f306b35d8e95d4c95c0bb748abeb
PierreTanZY <137426607+PierreTanZY@users.noreply.github.com>
2024-02-05 10:33:16 +0000

    updated test1.py

--- test1.py
+++ test1.py
@@ -1,12 +1,42 @@
 # following youtube tutorial, 'https://www.youtube.com/watch?v=AY9MnQ4x3zk'
 from sys import exit
 import pygame
+from random import randint
 
+# function to calculate/show score
 def display_score():
-    current_time = pygame.time.get_ticks() - start_time
-    score_surface = score_font.render(str(current_time), False, (64, 64, 64)).convert()
+    current_time = int((pygame.time.get_ticks() - start_time) / 1000)
+    score_surface = pixel_font.render(f'Score: {current_time}', False, (64, 64, 64)).convert()
     score_rect = score_surface.get_rect(center = (400, 50))
     screen.blit(score_surface, score_rect)
+    return current_time
+
+# function to move obstacles
+def obstacle_movement(obstacle_rect_list):
+    if obstacle_rect_list:
+        for obstacle_rect in obstacle_rect_list:
+            if obstacle_rect.x < -100:
+                obstacle_rect_list.remove(obstacle_rect)
+            else:
+                obstacle_rect.x -= 5
+
+            if obstacle_rect.bottom == 300:
+                screen.blit(snail_surface, obstacle_rect)
+            elif obstacle_rect.bottom == 150:
+                screen.blit(fly_surface, obstacle_rect)
+        return obstacle_rect_list
+    else:
+        return[]
+
+def collisions(player, obstacles):
+    if obstacles:
+        for obstacles_rect in obstacles:
+            if player.colliderect(obstacles_rect):
+                return False
+        return True
+    else:
+        return True
+
 
 pygame.init()
 screen = pygame.display.set_mode((800, 400))
@@ -18,19 +48,34 @@ player_gravity = 0
 player_isOnGround = False
 game_active = True
 start_time = 0
+highScore = 0
+currentScore = 0
+obstacle_rect_list = []
 
 # initialise font
-score_font = pygame.font.Font(('font/Pixeltype.ttf'), 50)
+pixel_font = pygame.font.Font(('font/Pixeltype.ttf'), 50)
 
 # initialise surfaces
 sky_surface = pygame.image.load('graphics/Sky.png').convert()
 ground_surface = pygame.image.load('graphics/ground.png').convert()
 snail_surface = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
+fly_surface = pygame.image.load('graphics/fly/fly1.png').convert_alpha()
 player_surface = pygame.image.load('graphics/player/player_walk_1.png').convert_alpha()
+playerStand_surface = pygame.image.load('graphics/player/player_stand.png').convert_alpha()
+playerStand_surface = pygame.transform.scale_by(playerStand_surface, 2)
+gameName_surface = pixel_font.render('Pixel Runner', False, (111, 196, 169)).convert()
+gameName_surface = pygame.transform.scale_by(gameName_surface, 2)
+gameMessage_surface = pixel_font.render('Press [spacebar] to run', False, (111, 196, 169))
 
 # initialise rectangles
-snail_rect = snail_surface.get_rect(midbottom = (800, 300))
 player_rect = player_surface.get_rect(midbottom = (80, 300))
+playerStand_rect = playerStand_surface.get_rect(center = (400,200))
+gameName_rect = gameName_surface.get_rect(center = (400, 80))
+gameMessage_rect = gameMessage_surface.get_rect(center = (400, 350))
+
+# Timer
+obstacleSpawn_timer = pygame.USEREVENT + 1
+pygame.time.set_timer(obstacleSpawn_timer, 1400)
 
 while True:
     # calls all events in the game and runs through it one by one
@@ -53,13 +98,19 @@ while True:
                 if event.key == pygame.K_SPACE:
                     if player_isOnGround:
                         player_gravity = -20
+
+            # spawn, move and delete obstacles
+            if event.type == obstacleSpawn_timer:
+                if randint(0,2):
+                    obstacle_rect_list.append(snail_surface.get_rect(midbottom = (randint(900,1100), 300)))
+                else:
+                    obstacle_rect_list.append(fly_surface.get_rect(midbottom = (randint(900,1100), 150)))
         
         # player is dead
         else:
             # restart game
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
-                    snail_rect.x = 800
                     game_active = True
                     start_time = pygame.time.get_ticks()
     
@@ -68,14 +119,9 @@ while True:
         # display surfaces
         screen.blit(sky_surface, (0,0))
         screen.blit(ground_surface, (0,300))
-        display_score()
-
-        # snail movement
-        if (snail_rect.x < -100): 
-            snail_rect.x = 800
-        else: 
-            snail_rect.left -= 4
-        screen.blit(snail_surface, snail_rect)
+        currentScore = display_score()
+        if currentScore > highScore:
+            highScore = currentScore
 
         # player
         player_gravity += 1
@@ -86,13 +132,33 @@ while True:
             player_isOnGround = True
         screen.blit(player_surface, player_rect)
 
+        # obstacle movement
+        obstacle_rect_list = obstacle_movement(obstacle_rect_list)
+
         # collision
-        if snail_rect.colliderect(player_rect):
-            game_active = False
+        game_active = collisions(player_rect, obstacle_rect_list)
     
     # player is dead
     else:
-        screen.fill('Black')
+        # reset obstacles and player
+        obstacle_rect_list.clear()
+        player_rect.midbottom = (80, 300)
+        player_gravity = 0
+
+        # display game over screen
+        screen.fill((94, 129, 162))
+        screen.blit(playerStand_surface, playerStand_rect)
+        screen.blit(gameName_surface, gameName_rect)
+        screen.blit(gameMessage_surface, gameMessage_rect)
+
+        # display score and highscore
+        currentScore_surface = pixel_font.render(f'Your score: {currentScore}', False, (111,196,169))
+        currentScore_rect = currentScore_surface.get_rect(center = (150, 200))
+        screen.blit(currentScore_surface, currentScore_rect)
+        highScore_surface = pixel_font.render(f'High score: {highScore}', False, (111, 196, 169))
+        highScore_rect = highScore_surface.get_rect(center = (650, 200))
+        screen.blit(highScore_surface, highScore_rect)
+    
         
 
     # update frames
