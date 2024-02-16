01fec674921bc914d8f29696a5e1206c6239a6e5
PierreTanZY <137426607+PierreTanZY@users.noreply.github.com>
2024-02-16 10:02:57 +0000

    updated dir 4_stardew_valley

--- 4_stardew_valley/code/settings.py
+++ 4_stardew_valley/code/settings.py
@@ -5,7 +5,7 @@ SCREEN_HEIGHT = 720
 TILE_SIZE = 64
 
 # overlay positions
-OVERLY_POSITIONS = {
+OVERLAY_POSITIONS = {
     'tool' : (40, SCREEN_HEIGHT - 15),
     'seed' : (70, SCREEN_HEIGHT - 5)
 }
